dd94befea3fb0d852f043410afa244b02c59ce27
Takashi Mori <kanetugu2015@gmail.com>
2025-11-03 12:54:44 +0900

    fix #36

--- src/hakoniwa_pdu/rpc/shm/shm_pdu_service_client_manager.py
+++ src/hakoniwa_pdu/rpc/shm/shm_pdu_service_client_manager.py
@@ -21,15 +21,18 @@ class ShmPduServiceClientManager(
         offmap = offset_map.create_offmap(self.offset_path)
         self.service_config = ServiceConfig(self.service_config_path, offmap, hakopy=hakopy)
 
-        pdudef = self.service_config.append_pdu_def(self.pdu_config.get_pdudef())
-        self.pdu_config.update_pdudef(pdudef)
-        print("Service PDU definitions prepared.")
-
+        tmp_pdudef = self.pdu_config.get_pdudef()
+        self.load_shared_memory_for_safe(tmp_pdudef)
         handle = hakopy.asset_service_client_create(
             self.asset_name, service_name, client_name
         )
         if handle is None:
             return None
+
+        pdudef = self.service_config.append_pdu_def(self.pdu_config.get_pdudef())
+        self.pdu_config.update_pdudef(pdudef)
+        print("Service PDU definitions prepared.")
+
         client_id = handle["client_id"]
         self.client_handles[client_id] = handle
         ids = hakopy.asset_service_get_channel_id(handle["service_id"], handle["client_id"])
--- src/hakoniwa_pdu/rpc/shm/shm_pdu_service_server_manager.py
+++ src/hakoniwa_pdu/rpc/shm/shm_pdu_service_server_manager.py
@@ -24,12 +24,15 @@ class ShmPduServiceServerManager(
         offmap = offset_map.create_offmap(self.offset_path)
         self.service_config = ServiceConfig(self.service_config_path, offmap, hakopy=hakopy)
 
-        pdudef = self.service_config.append_pdu_def(self.pdu_config.get_pdudef())
-        self.pdu_config.update_pdudef(pdudef)
-        print("Service PDU definitions prepared.")
+        tmp_pdudef = self.pdu_config.get_pdudef()
+        self.load_shared_memory_for_safe(tmp_pdudef)
         service_id = hakopy.asset_service_create(self.asset_name, service_name)
         if service_id < 0:
             return False
+
+        pdudef = self.service_config.append_pdu_def(self.pdu_config.get_pdudef())
+        self.pdu_config.update_pdudef(pdudef)
+        print("Service PDU definitions prepared.")
         self.service_id_map[service_id] = service_name
         return True
 
